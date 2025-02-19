dd24ce37b17498665af2261b2fe9756ea00b8a10
blink <blinkcode24@gmail.com>
2025-02-19 16:00:32 +0800

    fix styling and added navbar

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.querySelector("#title").innerText = "Faith, Beliefs, and Way of Life";
+document.querySelector("#subtitle").innerText =
+  "Understanding the History, Teachings, and Community of Jehovah's Witnesses";
+document.querySelector("#description").innerText =
+  "his website covers the beliefs and practices of Jehovah's Witnesses, a Christian group known for their door-to-door preaching and focus on Bible study. Discover their unique teachings, community life, and dedication to living a life based on faith, worship, and sharing the message of God's Kingdom. Whether you're interested in their worship or want to learn more about their views on spiritual and social topics, this guide provides a clear overview.";
+document.querySelector("#join-us-button-text").innerText = "Join Us";
